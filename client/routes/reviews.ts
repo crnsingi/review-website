bb738912347afeb8b8cf9173ced128a20a316421
Cesar <cesarnsingi@hotmail.com>
2024-12-07 12:30:04 +0100

    router get try

--- client/routes/reviews.ts
+++ client/routes/reviews.ts
@@ -2,5 +2,12 @@ import express from "express";
 const router = express.Router();
 import Review from "../models/Review";
 
+router.get('/review', async (req, res) => {
+    try{
+        const reviews = await Review.find();
+        res.json(reviews);
+    }
+})
+
 
 
