326282bd21051a4394be6ff87436576545264b4f
Cesar <cesarnsingi@hotmail.com>
2024-12-08 07:18:17 +0100

    add components and import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'express';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import AddReview from './components/AddReview';
 
 
 const App: React.FC = () => {
