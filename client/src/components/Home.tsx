ba8f7cd9047cc21d8360c7269c5aae001bc52fa6
Cesar <cesarnsingi@hotmail.com>
2024-12-08 07:25:44 +0100

    interface Review

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,3 +1,10 @@
 import React, {useEffect, useState} from "react";
 import axios  from "axios";
 import { Link } from "react-router-dom";
+
+interface Review { 
+    author: string;
+    content: string;
+    rating: number;
+    _id: string;
+}
